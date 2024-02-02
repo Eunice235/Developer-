b43b9273ba8640408389f0349b5b452a602aea61
Maina Waithira Eunice <110166836+Eunice235@users.noreply.github.com>
2024-02-02 19:38:07 +0300

    Update app.js
    cv editing

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ cvButton.addEventListener('click', () => {
   const downloadLink = document.createElement('a');
   
   // Set the href attribute to the location of your CV file
-  downloadLink.href = 'https://docs.google.com/document/d/1-hp4aqUzEbx8UUSr-uyw-coOKsbW8RwfK0ur6gCMvmQ/edit';
+  downloadLink.href = 'https://docs.google.com/document/d/12j6HQpnEhU3r6q8tATV0o94o-Dcc30Kl5jRbO_VdtHE/edit?pli=1';
   
   // Set the download attribute to specify the file name
   downloadLink.download = 'curriculumn vitae.pdf';
